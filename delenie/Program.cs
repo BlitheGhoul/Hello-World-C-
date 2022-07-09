6c059ac7d6f4f33cab141fc691b5a9f2eb00087b
Tosh <88.a.potapov@gmail.com>
2022-07-09 21:33:15 +0300

    add delenie and Sum

---
+++ delenie/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите делимое");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите делитель");
+int num2 = int.Parse(Console.ReadLine());
+int sum = num1 / num2;
+Console.WriteLine("Ваш пример: " + num1 + " " + ":" + " " + num2 + " " + "=" + " " +  sum);
