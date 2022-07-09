229d1c7eaeb851cfc81df2b0c23e5e4ad7b83f5f
Tosh <88.a.potapov@gmail.com>
2022-07-09 22:35:13 +0300

    add weights

---
+++ weights/Program.cs
@@ -0,0 +1,15 @@
+﻿int one = 1;
+int two = 3;
+int three = 8;
+int four = 2;
+int five = 6;
+int max = 1;
+
+if (one > max) max = one;
+if (two > max)  max = two;
+if (three > max) max = three;
+if (four > max) max = four;
+if (five > max) max = five;
+
+
+Console.WriteLine("Максимальное число: " + max);
