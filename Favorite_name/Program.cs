8e208947a9f436d7c4a590676d8f3ff227ab9105
Tosh <88.a.potapov@gmail.com>
2022-07-09 22:09:24 +0300

    add Favorite name

---
+++ Favorite_name/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите Ваше имя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "маша")
+{
+    Console.WriteLine ("О! Да это ж Маша!");
+}
+else
+{
+    Console.WriteLine ("Привет, " + name);
+}
