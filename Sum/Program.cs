cb059699778d8089728bb4f4f4d17202f5083051
Tosh <88.a.potapov@gmail.com>
2022-07-09 21:43:53 +0300

    add random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Введите первое число для сложения");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число для сложения");
-int num2 = int.Parse(Console.ReadLine());
+﻿//Console.WriteLine("Введите первое число для сложения");
+int num1 = new Random().Next(1, 10);         //int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число для сложения");
+int num2 = new Random().Next(1, 10);         //int.Parse(Console.ReadLine());
 int sum = num1 + num2;
 Console.WriteLine("Ваш пример: " + num1 + " " + "+" + " " + num2 + " " + "=" + " " +  sum);
